e72a146ca9fe7fcae8bbfd981d947ec0f1273573
Monirul Islam Mahmud <mahmudislam2025@gmail.com>
2023-06-22 19:08:21 +0600

    All done

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # save this as app.py
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 import pickle
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -7,6 +10,73 @@ scaler = StandardScaler()
 
 model = pickle.load(open('model_pickle.pkl', 'rb'))
 app = Flask(__name__)
+
+app.secret_key = 'xyzsdfg'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'stroke'
+
+mysql = MySQL(app)
+
+# @app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password, ))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            return render_template('index.html', mesage = mesage)
+        else:
+            mesage = 'Please enter correct email / password !'
+    return render_template('login.html', mesage = mesage)
+
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+
+
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form :
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password, ))
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage = mesage)
+
+
+
 
 @app.route('/about')
 def about():
@@ -17,7 +87,7 @@ def contact():
     return render_template("contact.html")
 
     
-@app.route('/analysis')
+@app.route('/analysis', methods=['GET', 'POST'])
 def analysis():
 
     if request.method =="POST":
@@ -102,15 +172,15 @@ def analysis():
 
         prediction = model.predict(feature)
         
-        return render_template('result.html', prediction=prediction, gender=gender, age=age, hypertension=hypertension, heart_disease=heart_disease, ever_married_Yes=ever_married_Yes, work=work, residence=residence, avg_glucose_level=avg_glucose_level, bmi=bmi, smoking=smoking)
-
+        #return render_template('result.html', prediction=prediction, gender=gender, age=age, hypertension=hypertension, heart_disease=heart_disease, ever_married_Yes=ever_married_Yes, work=work, residence=residence, avg_glucose_level=avg_glucose_level, bmi=bmi, smoking=smoking)
+        return render_template("stroke.html")
     else:
         return render_template("stroke.html")
     
     
     
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method =="POST":
         gender = request.form['gender']
@@ -194,10 +264,9 @@ def home():
 
         prediction = model.predict(feature)
         
-        return render_template('result.html', prediction=prediction, gender=gender, age=age, hypertension=hypertension, heart_disease=heart_disease, ever_married_Yes=ever_married_Yes, work=work, residence=residence, avg_glucose_level=avg_glucose_level, bmi=bmi, smoking=smoking)
-
+        #return render_template('result.html', prediction=prediction, gender=gender, age=age, hypertension=hypertension, heart_disease=heart_disease, ever_married_Yes=ever_married_Yes, work=work, residence=residence, avg_glucose_level=avg_glucose_level, bmi=bmi, smoking=smoking)
+        return render_template("stroke.html")
     else:
-        #return render_template("stroke.html")
         return render_template("index.html")
 
 
